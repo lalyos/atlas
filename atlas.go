be838a0db4fdb8b23ed777e4978472b6adae1631
lalyos <lalyos@yahoo.com>
2016-01-27 16:26:07 +0100

    initial version

---
+++ atlas.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"text/template"
+
+	"github.com/codegangsta/cli"
+	"github.com/hashicorp/atlas-go/v1"
+)
+
+func init() {
+	log.SetOutput(ioutil.Discard)
+}
+
+func ToJson(obj interface{}) (interface{}, error) {
+	data, err := json.Marshal(obj)
+	if err != nil {
+		return nil, err
+	}
+	return string(data), nil
+}
+
+func search(c *cli.Context) {
+	searchOpts := &atlas.ArtifactSearchOpts{
+		User: c.String("user"),
+		Name: c.String("artifact"),
+		Type: c.String("type"),
+	}
+	if len(c.StringSlice("meta")) > 0 {
+		filter := map[string]string{}
+		for _, m := range c.StringSlice("meta") {
+			pair := strings.Split(m, "=")
+			filter[pair[0]] = pair[1]
+		}
+		fmt.Printf("FILTER: %#v\n", filter)
+		searchOpts.Metadata = filter
+	}
+	client := atlas.DefaultClient()
+
+	versions, err := client.ArtifactSearch(searchOpts)
+	if err != nil {
+		fmt.Errorf("search error: %#v", err)
+		os.Exit(1)
+	}
+
+	fnMap := template.FuncMap{}
+	fnMap["json"] = ToJson
+	tmpl, err := template.New("artifact").Funcs(fnMap).Parse(c.String("format"))
+	if err != nil {
+		panic(err)
+	}
+
+	for _, v := range versions {
+		//fmt.Println("ver: %#v", v)
+		err = tmpl.Execute(os.Stdout, v)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+}
+
+func main() {
+	fmt.Println("Search atlas.hashicorp artifacts ...")
+
+	app := cli.NewApp()
+	app.Name = "atlifacts"
+	app.Usage = "query atlas.hashicorp.com artifacts"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "user, u",
+			Value:  "sequenceiq",
+			Usage:  "atlas user",
+			EnvVar: "ATLAS_USER",
+		},
+		cli.StringFlag{
+			Name:   "artifact, a",
+			Value:  "cloudbreak",
+			Usage:  "atlas artifact",
+			EnvVar: "ATLAS_ARTIFACT_NAME",
+		},
+		cli.StringFlag{
+			Name:   "type, t",
+			Value:  "openstack.image",
+			Usage:  "atlas artifact type",
+			EnvVar: "ATLAS_ARTIFACT_TYPE",
+		},
+		cli.StringFlag{
+			Name:  "format, f",
+			Value: "{{.Slug}}\n",
+			Usage: "output format in golang template",
+		},
+		cli.StringSliceFlag{
+			Name:  "meta, m",
+			Usage: "meta field as fielter",
+		},
+	}
+	app.Action = search
+
+	app.Run(os.Args)
+
+}
